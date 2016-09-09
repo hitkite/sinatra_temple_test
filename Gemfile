6ff20fd2bdf33091712daf4356ecb1860d301580
hitkite <hitkite@gmail.com>
2016-09-09 14:55:49 +0000

    pg

--- Gemfile
+++ Gemfile
@@ -1,12 +1,16 @@
 # Gemfile
 source 'https://rubygems.org'
 
+gem 'activerecord'
 gem 'sinatra'
+gem 'sinatra-activerecord'
 
+gem 'rake'
 # Assets
 gem 'haml'
 gem 'sass'
 gem 'coffee-script'
+gem 'pg'
 
 group :development do
   gem 'foreman'
--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 # app.rb
 require 'bundler/setup'
 require 'sinatra'
+require 'activerecord'
 
 require 'haml'
 require 'sass'
 require 'coffee-script'
 
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/app.db')
+
+class Count < ActiveRecord::Base; end
+  
 get '/' do
   @mes = 'Sinatar de Hello!'
   haml :index
